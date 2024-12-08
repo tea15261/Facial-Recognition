f8ceebce74e738fb45dc01034f92745c20ae773d
Atiksh Jha <atikshjha2007@gmail.com>
2024-12-08 01:16:23 -0500

    Imported standard dependencies

---
+++ main.py
@@ -0,0 +1,10 @@
+import cv2
+import os
+import random
+import numpy as np
+from matplotlib import pyplot as plt
+
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Layer, Conv2D, Dense, MaxPooling2D, Input, Flatten
+import tensorflow as tf
+
