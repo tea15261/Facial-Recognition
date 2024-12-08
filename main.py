19cac8cb679a781ba3928c54befa0bc5f08e25a6
Atiksh Jha <atikshjha2007@gmail.com>
2024-12-08 11:44:45 -0500

    added comments

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+# Import standard dependencies
 import cv2
 import os
 import random
 import numpy as np
 from matplotlib import pyplot as plt
 
+# Import tensorflow dependencies - Functional API
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Layer, Conv2D, Dense, MaxPooling2D, Input, Flatten
 import tensorflow as tf
